3fdacdc87c42fac47d0e204d1a074200945bf8ae
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-16 02:01:33 -0300

    Update declarando-variaveis.c

--- declarando-variaveis.c
+++ declarando-variaveis.c
@@ -4,19 +4,19 @@
 #include <limits.h>
 
 int main() {
-	setlocale(LC_ALL, "portuguese");
+    setlocale(LC_ALL, "portuguese");
 	
-	// Declarar vari·veis.
-	int numInteiro = 15;      // Inteiro
+    // Declarar vari√°veis.
+    int numInteiro = 15;      // Inteiro
     float numReal = 5.99;     // Real
     char letra = 'D';         // Character
     char cadeia[200] = "String";   // Cadeia
 
-    // Exibir vari·veis.
+    // Exibir vari√°veis.
     printf("%d \n", numInteiro);
     printf("%.2f \n", numReal);
     printf("%c \n", letra);
     printf("%s \n", cadeia);
 	
-	return 0;
+    return 0;
 }
