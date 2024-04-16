af50a602a8b66cfe1457d57a930509b4ce55bb4d
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-16 03:09:42 -0300

    Create break-pause.c

---
+++ break-pause.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "portuguese");			  
+    
+    int i;
+   
+  for (i = 0; i < 10; i++) {
+       if (i == 4) {
+       break;
+  }
+  printf("%d \n", i);
+}
+
+   printf("\n");
+ 
+   for (i = 0; i < 10; i++) {
+        if (i == 4) {
+        continue;
+  }
+  printf("%d \n", i);
+}
+
+	return 0;
+	
+	}
