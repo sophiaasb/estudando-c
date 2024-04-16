a109e6223c170909f1831987ce14cc7d90a5e5f5
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-16 03:15:29 -0300

    Create elementos-vetor.c

---
+++ elementos-vetor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "portuguese");			  
+
+    int vetor[5];
+    int i;
+
+    printf("Digite os elementos do vetor: \n");
+    for (i = 0; i < 5; i++) {
+
+    printf("Elemento %d: ", i + 1);
+    scanf("%d", &vetor[i]);
+}
+
+    system("cls ||clear");
+    for (i = 0; i < 5; i++) {
+    printf("Elemento %d: %d \n", i+1, vetor[i]);
+}
+	return 0;
+	
+	}
