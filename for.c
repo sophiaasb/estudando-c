3880eb0ec22948616d89f4a229a0b89560b19b0b
sophiaasb <161032086+sophiaasb@users.noreply.github.com>
2024-04-16 03:02:11 -0300

    Create for.c

---
+++ for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "portuguese");			  
+ 
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("%d\n", i);
+}
+    
+	return 0;
+	
+	}
